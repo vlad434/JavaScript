78135d0b121c6a0d4f749085b63f55c22f8f826e
vlady434 <vladilie06@yahoo.com>
2022-01-21 19:49:45 +0200

    second commit

--- Dollars to cents converter/app.js
+++ Dollars to cents converter/app.js
@@ -1,8 +1,8 @@
 const coins = document.querySelectorAll('.coin');
 document.querySelector('.btn-convert').addEventListener('click', Convert);
+const errorEl = document.querySelector('.error-message');
 function Convert(e) {
   let number = document.getElementById('number').value;
-
   let totalCents = document.getElementById('total-cents');
   let quarters = document.getElementById('quarters');
   let dimes = document.getElementById('dimes');
@@ -15,6 +15,10 @@ function Convert(e) {
     dimes.value = '';
     nickels.value = '';
     pennies.value = '';
+    errorEl.style.transform = 'translateY(0vh)';
+    setTimeout(() => {
+      errorEl.style.transform = 'translateY(-9vh)';
+    }, 1800);
   } else {
     totalCents.value = (number * 100).toFixed(0);
     quarters.value = Math.floor(number * 4);
@@ -28,13 +32,13 @@ function Convert(e) {
       dimes.value * 10 -
       nickels.value * 5;
 
-    coins.forEach((coin) => {
-      if (coin) {
-        coin.setAttribute('class', 'coin animate-heads');
-      } else {
-        coin.setAttribute('class', 'coin animate-tails');
-      }
-    });
+    // coins.forEach((coin) => {
+    //   if (coin) {
+    //     coin.setAttribute('class', 'coin animate-heads');
+    //   } else {
+    //     coin.setAttribute('class', 'coin animate-tails');
+    //   }
+    // });
   }
 
   e.preventDefault();
