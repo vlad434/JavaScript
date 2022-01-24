588773b380c3d86666ba6333105d28b6f07c6009
vlady434 <vladilie06@yahoo.com>
2022-01-24 14:42:16 +0200

    first commit

--- Notes App/app.js
+++ Notes App/app.js
@@ -1,61 +1,11 @@
-let addBtn = document.querySelector('.add-btn');
-addBtn.addEventListener('click', function (e) {
-  formContainer.style.display = 'block';
-  e.preventDefault();
+const addBtn = document.getElementById('add');
+const addForm = document.getElementById('add-form');
+const closeBtn = document.getElementById('close-btn');
+
+addBtn.addEventListener('click', () => {
+  addForm.style.opacity = '1';
 });
 
-//NOTE SELECTORS
-let deleteBtn = document.getElementById('delete-btn');
-let note = document.querySelector('.note');
-
-//FORM SELECTORS
-let formContainer = document.querySelector('.form-container');
-let titleForm = document.getElementById('note-title-input');
-let textForm = document.getElementById('note-text');
-let cancelBtn = document.getElementById('cancel-btn');
-let createBtn = document.getElementById('create-btn');
-
-createBtn.addEventListener('click', function (e) {
-  if (titleForm.value === '' || textForm.value === '') {
-    titleForm.style.backgroundColor = 'rgba(255,0,0,0.5)';
-    textForm.style.backgroundColor = 'rgba(255,0,0,0.5)';
-
-    setTimeout(() => {
-      titleForm.style.backgroundColor = 'none';
-      textForm.style.backgroundColor = 'none';
-    }, 1000);
-  } else {
-    formContainer.style.display = 'none';
-    displayNote();
-  }
-  e.preventDefault();
-});
-
-cancelBtn.addEventListener('click', (e) => {
-  titleForm.value = '';
-  textForm.value = '';
-  formContainer.style.display = 'none';
-  e.preventDefault();
-});
-
-let page = document.getElementById('page-content');
-
-function displayNote() {
-  let output = `<div class="note">
-          <div class="box-border">${titleForm.value}</div>
-          <hr />
-          <div class="note-content">
-              <div class="text-content">${textForm.value}</div>
-              <div class="note-options">
-                <button id="delete-btn" class="fa fa-trash">x</button>
-              </div>
-          </div>
-        </div>
-        `;
-  page.innerHTML += output;
-}
-
-deleteBtn.addEventListener('click', (e) => {
-  note.remove();
-  e.preventDefault();
+closeBtn.addEventListener('click', () => {
+  addForm.style.opacity = '0';
 });
