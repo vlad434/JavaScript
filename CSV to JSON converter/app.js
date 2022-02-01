9d2bcedbe40b7292f3fd91b2d753719005da83ea
vlady434 <vladilie06@yahoo.com>
2022-02-01 16:29:25 +0200

    another design

--- CSV to JSON converter/app.js
+++ CSV to JSON converter/app.js
@@ -1,60 +1,54 @@
-const alertContainer = document.querySelector(".alert");
-const convertInCSV = document.getElementById("convert-to-csv");
-const input = document.getElementById("input");
-const output = document.getElementById("textarea");
-const uploadBtn = document.getElementById("upload");
-const downloadBtn = document.getElementById("download-btn");
-const clearBtn = document.getElementById("clear-btn");
-const copyBtn = document.getElementById("copy-btn");
+const alertContainer = document.querySelector('.alert');
+const convertInCSV = document.getElementById('convert-to-csv');
+const input = document.getElementById('input');
+const output = document.getElementById('textarea');
+const uploadBtn = document.getElementById('upload');
+const downloadBtn = document.getElementById('download-btn');
+const clearBtn = document.getElementById('clear-btn');
+const copyBtn = document.getElementById('copy-btn');
 
-// output.disabled = true;
-
-convertInCSV.addEventListener("click", convertToCSV);
-clearBtn.addEventListener("click", clearAll);
+convertInCSV.addEventListener('click', convertToCSV);
+clearBtn.addEventListener('click', clearAll);
 
 function convertToCSV() {
   if (validate()) {
-    let csvText = "";
+    let csvText = '';
     jsonText = input.value;
-    jsonText = jsonText.replace(/[\r\s\n\]\[\{"]/g, "");
-    jsonText = jsonText.replace(/},/g, "}");
-    // console.log(jsonText);
+    jsonText = jsonText.replace(/[\r\s\n\]\[\{"]/g, '');
+    jsonText = jsonText.replace(/},/g, '}');
     let variables;
-    variables = jsonText.split("}");
-    // console.log(variables);
+    variables = jsonText.split('}');
     for (let i = 0; i < variables.length - 1; i++) {
       let currVar;
       currVar = variables[i];
-      // console.log(variables[1]);
-      vars = currVar.split(",");
+      vars = currVar.split(',');
 
       for (let j = 0; j < vars.length; j++) {
-        vars[j] = vars[j].split(":");
+        vars[j] = vars[j].split(':');
       }
 
       for (let j = 0; j < vars.length; j++) {
         if (i == 0) {
           csvText += `${vars[j][0]}`;
           if (j < vars.length - 1) {
-            csvText += ",";
+            csvText += ',';
           } else {
-            csvText += "\n";
+            csvText += '\n';
           }
         }
       }
       for (let j = 0; j < vars.length; j++) {
         csvText += `${vars[j][1]}`;
         if (j < vars.length - 1) {
-          csvText += ",";
+          csvText += ',';
         } else {
-          csvText += "\n";
+          csvText += '\n';
         }
       }
       output.value = csvText;
     }
   } else {
-    //create alert
-    alertMessage("Invalid input!");
+    alertMessage('Invalid input!');
   }
 
   function validate() {
@@ -68,34 +62,30 @@ function convertToCSV() {
   }
 }
 
-//   create alert function
 function alertMessage(message) {
-  const alert = document.createElement("h1");
+  const alert = document.createElement('h1');
   alert.textContent = message;
   alertContainer.appendChild(alert);
 
   setTimeout(function () {
-    document.querySelector(".alert").remove();
+    document.querySelector('.alert').remove();
     window.location.reload();
   }, 1000);
 }
 
-//clear form button
 function clearAll() {
-  input.value = "";
-  output.value = "";
+  input.value = '';
+  output.value = '';
 }
 
-//copy button
 function copyText() {
   document.querySelector('textarea[name="output"]').select();
-  document.execCommand("copy");
+  document.execCommand('copy');
 }
 
-//download button
 function downloadFile() {
   var textt = output.value;
-  var blob = new Blob([textt], { type: "text/plain;charset=utf-8" });
-  FileSaver.saveAs(blob, "hello world.txt");
+  var blob = new Blob([textt], { type: 'text/plain;charset=utf-8' });
+  FileSaver.saveAs(blob, 'hello world.txt');
   console.log(textt);
 }
