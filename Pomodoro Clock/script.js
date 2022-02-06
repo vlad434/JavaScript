28a739309e5b022e75ba79adc32badbe0781d47c
vlady434 <vladilie06@yahoo.com>
2022-02-06 10:57:22 +0200

    almost done

--- Pomodoro Clock/script.js
+++ Pomodoro Clock/script.js
@@ -1,3 +1,6 @@
+let Title = document.querySelector('title');
+let title = document.querySelector('.title');
+
 //CLOCK CONTAINER SELECTORS
 const increase = document.getElementById('increase');
 let minutes = document.querySelector('.minutes');
@@ -5,52 +8,102 @@ let seconds = document.querySelector('.seconds');
 const decrease = document.getElementById('decrease');
 
 //BUTTONS SELECTORS
-const s_btn = document.querySelector('.session-buttons');
+const session_buttons = document.querySelector('.session-buttons');
+const break_buttons = document.querySelector('.break-container');
 const startBtn = document.getElementById('start-btn');
 const stopBtn = document.getElementById('stop-btn');
 const resetBtn = document.getElementById('reset-btn');
+const skipBtn = document.querySelector('.skip-btn');
+const breakBtn = document.getElementById('break-btn');
+const skipBtnBreak = document.querySelector('.skip-btn-break');
 
-let startTimerSession;
+let Session;
+let Break;
+let sessionMin = 25;
+let current = 'session';
 
+// DECREASE TIME SESSION
 decrease.addEventListener('click', () => {
-  if (minutes.innerText > 00) {
+  if (minutes.innerText <= '01') {
+    minutes.style.color = 'red';
+    seconds.style.color = 'red';
+    setTimeout(() => {
+      minutes.style.color = '#fff';
+      seconds.style.color = '#fff';
+    }, 800);
+  } else {
     minutes.innerText--;
     formatTime(minutes);
-  } else return;
-}); 
-
-increase.addEventListener('click', () => {
-  minutes.innerText++;
-  formatTime(minutes);
-});
-
-startBtn.addEventListener('click', () => {
-  startBtn.style.display = 'none';
-  s_btn.style.display = 'block';
-  if (startTimerSession === undefined) {
-    startTimerSession = setInterval(startTimer, 100);
-  } else {
-    console.log('Clock is already running!');
   }
 });
 
+// INCREASE TIME SESSION
+increase.addEventListener('click', () => {
+  if (minutes.innerText < '60') {
+    minutes.innerText++;
+    formatTime(minutes);
+  } else {
+    minutes.style.color = 'red';
+    seconds.style.color = 'red';
+    setTimeout(() => {
+      minutes.style.color = '#fff';
+      seconds.style.color = '#fff';
+    }, 800);
+  }
+});
+
+// START BUTTON
+startBtn.addEventListener('click', () => {
+  startBtn.style.display = 'none';
+  Session = setInterval(startTimer, 1000);
+  session_buttons.style.display = 'block';
+});
+
+// STOP BUTTON
 stopBtn.addEventListener('click', () => {
-  stopTimer();
-  startTimerSession = undefined;
-  s_btn.style.display = 'none';
+  stopTimer(Session);
+  session_buttons.style.display = 'none';
   startBtn.style.display = 'block';
 });
 
+//RESET BUTTON
 resetBtn.addEventListener('click', () => {
-  resetFunc();
-  stopTimer();
-  startTimerSession = undefined;
+  reset();
+  stopTimer(Session);
 });
 
+//SKIP BREAK BUTTON
+skipBtn.addEventListener('click', () => {
+  reset();
+  stopTimer(Session);
+  break_buttons.style.display = 'none';
+  title.innerText = 'Session';
+  updateTitle();
+});
+
+skipBtnBreak.addEventListener('click', () => {
+  reset();
+  stopTimer(Break);
+  title.innerText = 'Session';
+  updateTitle();
+});
+
+//BREAK BUTTON - ENTER IN BREAK TIME
+breakBtn.addEventListener('click', () => {
+  title.innerText = 'Break';
+  break_buttons.style.display = 'none';
+  skipBtnBreak.style.display = 'block';
+  Break = setInterval(startTimer, 1000);
+  increase.disabled = true;
+  decrease.disabled = true;
+});
+
+//FORMAT TIME FUNCTION
 function formatTime(time) {
-  time.innerText = time.innerText < 10 ? '0' + time.innerText : time.innerText;
+  time.innerText = time.innerText > 9 ? time.innerText : `0${time.innerText}`;
 }
 
+//START TIMER FUNCTION
 function startTimer() {
   if (seconds.innerText != 0) {
     seconds.innerText--;
@@ -59,22 +112,52 @@ function startTimer() {
     seconds.innerText = 59;
     minutes.innerText--;
     formatTime(minutes);
+    increase.disabled = true;
+    decrease.disabled = true;
   }
 
-  if (minutes.innerText == 00 && seconds.innerText == 00) {
-    stopTimer();
+  if (current == 'session') {
+    if (minutes.innerText == 00 && seconds.innerText == 00) {
+      stopTimer(Session);
+      current = 'break';
+      title.innerText = 'Break';
+      minutes.innerText = '25';
+      session_buttons.style.display = 'none';
+      break_buttons.style.display = 'block';
+    }
+  } else {
+    if (minutes.innerText == 00 && seconds.innerText == 00) {
+      title.innerText = 'Session';
+      stopTimer(Break);
+      reset();
+    }
   }
+  updateTitle();
 }
 
-function stopTimer() {
-  clearInterval(startTimerSession);
-  console.log('stop');
+//STOP CLOCK FUNCTION
+function stopTimer(timer) {
+  clearInterval(timer);
+  // if (current === 'session') {
+  //   clearInterval(Session);
+  // } else {
+  //   clearInterval(Break);
+  // }
+  // console.log('stop');
 }
 
-function resetFunc() {
-  minutes.innerText = '25';
+//RESET FUNCTION
+function reset() {
+  minutes.innerText = sessionMin;
   seconds.innerText = '00';
-  stopTimer();
-  s_btn.style.display = 'none';
+  increase.disabled = false;
+  decrease.disabled = false;
+  session_buttons.style.display = 'none';
+  skipBtnBreak.style.display = 'none';
   startBtn.style.display = 'block';
+  updateTitle();
+}
+
+function updateTitle() {
+  Title.innerHTML = `${title.innerText} - (${minutes.innerText}:${seconds.innerText}) - Pomodoro Clock`;
 }
