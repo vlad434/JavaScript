96f1db4f1fad99bf8f2f71db251cf721897e01e0
vlady434 <vladilie06@yahoo.com>
2022-02-26 11:53:37 +0200

    final touch-ups

--- Bin2Dec/app.js
+++ Bin2Dec/app.js
@@ -1,31 +1,62 @@
-let binaryValue = document.getElementById('binary');
-let decimalValue = document.getElementById('decimal');
+let input = document.querySelector('.num1');
+let output = document.querySelector('.num2');
 let errorDiv = document.querySelector('.msg');
+let convertBtn = document.getElementById('convert');
+let B2D = document.getElementById('bin-to-dec');
+let D2B = document.getElementById('dec-to-bin');
+
+convertBtn.addEventListener('click', () => {
+  if (input.placeholder === 'Binary') {
+    binaryToDecimal();
+  } else {
+    decimalToBinary();
+  }
+});
+
+B2D.addEventListener('click', () => {
+  input.setAttribute('placeholder', 'Binary');
+  input.setAttribute('id', 'binary');
+  output.setAttribute('placeholder', 'Decimal');
+  output.setAttribute('id', 'decimal');
+  convertBtn.innerText = 'Convert to decimal';
+  input.value = '';
+  output.value = '';
+});
+
+D2B.addEventListener('click', () => {
+  input.setAttribute('placeholder', 'Decimal');
+  input.setAttribute('id', 'decimal');
+  output.setAttribute('placeholder', 'Binary');
+  output.setAttribute('id', 'binary');
+  convertBtn.innerText = 'Convert to binary';
+  input.value = '';
+  output.value = '';
+});
 
 function binaryToDecimal() {
-  let binar = binaryValue.value;
-  let lastChr = binar.slice(-1);
+  let Input = input.value;
+  let lastChr = Input.slice(-1);
   if (lastChr == '') {
     errMsg('Please input a number.');
     return;
   }
   if (lastChr == 1 || lastChr == 0) {
-    decimalValue.value = parseInt(binaryValue.value, 2);
+    output.value = parseInt(input.value, 2);
   } else {
     errMsg(`"${lastChr}" is not valid input.`);
   }
 }
 
 function decimalToBinary() {
-  let decimal = decimalValue.value;
-  let lastChr = decimal.slice(-1);
+  let Input = input.value;
+  let lastChr = Input.slice(-1);
   if (lastChr == '') {
     errMsg('Please enter a decimal number.');
     return;
-  } else if (isNaN(lastChr) || isNaN(decimal)) {
+  } else if (isNaN(lastChr) || isNaN(Input)) {
     errMsg(`"${lastChr}" is not a number.`);
   } else {
-    binaryValue.value = Number(decimal).toString(2);
+    output.value = Number(Input).toString(2);
   }
 }
 
@@ -33,10 +64,7 @@ function errMsg(msg) {
   errorDiv.innerText = `${msg}`;
   setTimeout(() => {
     errorDiv.innerText = '';
-    binaryValue.value = '';
-    decimalValue.value = '';
-  }, 1200);
+    input.value = '';
+    output.value = '';
+  }, 900);
 }
-
-binaryValue.addEventListener('input', binaryToDecimal);
-decimalValue.addEventListener('input', decimalToBinary);
