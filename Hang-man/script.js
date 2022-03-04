2ff273dd2c667cea5a0879c4eb4cb43436292ac8
vlady434 <vladilie06@yahoo.com>
2022-03-04 13:08:23 +0200

    another design

--- Hang-man/script.js
+++ Hang-man/script.js
@@ -1,9 +1,9 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+const playAgainBtn = document.getElementById('play-button');
 
 const figureParts = document.querySelectorAll('.figure-part');
 
